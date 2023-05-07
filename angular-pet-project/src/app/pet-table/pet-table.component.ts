c6534599c8c547a7605ce1652acf98bd76e9546c
sofikin <sofia.kalpin@khk.ee>
2023-05-07 16:58:40 +0300

    Pet table component to retrieve data from API

--- angular-pet-project/src/app/pet-table/pet-table.component.ts
+++ angular-pet-project/src/app/pet-table/pet-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 interface Pet {
   name: string;
@@ -13,6 +14,21 @@ interface Pet {
   templateUrl: './pet-table.component.html',
   styleUrls: ['./pet-table.component.css']
 })
-export class PetTableComponent {
+export class PetTableComponent implements OnInit {
+  pet: Pet[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  // Loads pets from API
+  ngOnInit() {
+    this.http.get<Pet[]>('http://localhost:8080/pet').subscribe(pet => {
+      this.pet = pet;
+    });
+  }
+
+  // Add new pet to Array
+  onPetAdded(newPet: Pet) {
+    this.pet.push(newPet);
+  }
 }
+
