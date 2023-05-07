ddd132296ebb7868aaca1a6647631ecbef1db342
sofikin <sofia.kalpin@khk.ee>
2023-05-07 17:03:21 +0300

    Pet form component to add data to API

--- angular-pet-project/src/app/pet-form/pet-form.component.ts
+++ angular-pet-project/src/app/pet-form/pet-form.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface NewPet {
+  name: string;
+  identificationCode: string;
+  type: string;
+  furColor: string;
+  country: string;
+}
 
 @Component({
   selector: 'app-pet-form',
@@ -6,5 +15,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./pet-form.component.css']
 })
 export class PetFormComponent {
+  newPet: NewPet = {
+    name: '',
+    identificationCode: '',
+    type: '',
+    furColor: '',
+    country: ''
+  };
+  @Output() petAdded = new EventEmitter<NewPet>();
 
+  constructor(private http: HttpClient) {}
+
+  // Add new pet to API
+  onSubmit() {
+    this.http.post<NewPet>('http://localhost:8080/pet', this.newPet).subscribe(newPet => {
+      this.petAdded.emit(newPet);
+      this.newPet = {
+        name: '',
+        identificationCode: '',
+        type: '',
+        furColor: '',
+        country: ''
+      }
+    });
+  }
 }
