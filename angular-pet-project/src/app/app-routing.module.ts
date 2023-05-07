b8d7142a97046fc01ce4e40b1d5a242d7ca267f9
sofikin <sofia.kalpin@khk.ee>
2023-05-07 17:12:37 +0300

    Angular router module

--- angular-pet-project/src/app/app-routing.module.ts
+++ angular-pet-project/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PetTableComponent} from "./pet-table/pet-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/pet', pathMatch: 'full' },
+  { path: 'pet', component: PetTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
